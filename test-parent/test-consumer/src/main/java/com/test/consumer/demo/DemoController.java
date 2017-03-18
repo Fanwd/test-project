4ab25c1224d548e8eb134ea91074e5c0189b9f7e
fanweidong <fanweidong2014@163.com>
2017-03-18 22:43:01 +0800

    测试dubbo服务

--- test-parent/test-consumer/src/main/java/com/test/consumer/demo/DemoController.java
+++ test-parent/test-consumer/src/main/java/com/test/consumer/demo/DemoController.java
@@ -1,14 +1,27 @@
 package com.test.consumer.demo;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.test.common.DemoService;
+
 @RestController
 public class DemoController {
+	
+	@Autowired
+	DemoService demoService = null;
 
+	private static Logger LOG = LoggerFactory.getLogger(DemoController.class);
 	
 	@RequestMapping(value="/test")
 	public String test(){
+		LOG.info("--------------------------------------dubbotest------------------------------------");
+		demoService.sayHello();
 		return "OK";
 	}
 }
